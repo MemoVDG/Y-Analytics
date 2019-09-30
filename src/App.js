61cbc8d5ae2a28df523cc9a71c8d4f5fb2821d8d
MemoVDG <memovdg@gmail.com>
2019-09-30 17:57:18 -0500

    Change color and added logo

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import logo from './images/logo.png'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
@@ -9,17 +9,6 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
