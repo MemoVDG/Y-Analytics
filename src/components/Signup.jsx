63f18354b14544578b0909dd1b822ba5c3e135a9
MemoVDG <memovdg@gmail.com>
2019-10-02 17:50:29 -0500

    Control data in signup

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -2,25 +2,34 @@ import React from 'react';
 import { Form, Button, Card } from 'react-bootstrap';
 
 export default function Login() {
+
+    const handleChange = (e) =>{
+        console.log(e)    
+    }
+
+    const handleSubmit = (e) =>{
+        console.log(e)
+    }
+
     return (
         <div className="App-header">
             <Card style={{ width: '30rem' }}>
 
-                <Form className="m-3 white">
+                <Form className="m-3 white" onSubmit = {handleSubmit}>
 
                     <Form.Group controlId="formBasicEmail">
                         <Form.Label style={{'color' : 'black'}} >Email address</Form.Label>
-                        <Form.Control type="email" placeholder="Enter email" />
+                        <Form.Control type="email" placeholder="Enter email" onChange={handleChange} />
                     </Form.Group>
 
                     <Form.Group controlId="formBasicPassword">
                         <Form.Label style={{'color' : 'black'}}>Password</Form.Label>
-                        <Form.Control type="password" placeholder="Password" />
+                        <Form.Control type="password" placeholder="Password" onChange={handleChange} />
                     </Form.Group>
 
                     <Form.Group controlId="formBasicEmail">
                         <Form.Label style={{'color' : 'black'}} >Full Name</Form.Label>
-                        <Form.Control type="email" placeholder="Enter email" />
+                        <Form.Control type="email" placeholder="Enter email" onChange={handleChange} />
                     </Form.Group>
 
                     <Form.Group controlId="formBasicCheckbox">
