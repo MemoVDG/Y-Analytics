334fde36f7f4ab42ff335e9cb0665b4b1e434345
MemoVDG <memovdg@gmail.com>
2019-10-04 12:28:15 -0500

    Redux integration

--- src/index.js
+++ src/index.js
@@ -3,21 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import WebFont from 'webfontloader';
-import { createStore, applyMiddleware } from 'redux';
-import rootReducer from './store/reducers/rootReducer';
 import {Provider} from 'react-redux'
 
-import thunk from 'redux-thunk';
-
-WebFont.load({
-  google: {
-    families: ['Gloria Hallelujah', 'cursive']
-  }
-});
+import configureStore from './configureStore';
 
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
+
+
+const store = configureStore();
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 
