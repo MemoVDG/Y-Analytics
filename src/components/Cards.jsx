fae12d697b80471023aa41eda42be1c2b6203ae9
MemoVDG <memovdg@gmail.com>
2019-10-07 13:45:14 -0500

    Display data from Youtube API

---
+++ src/components/Cards.jsx
@@ -0,0 +1,69 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import IconButton from "@material-ui/core/IconButton";
+import Typography from "@material-ui/core/Typography";
+import SkipPreviousIcon from "@material-ui/icons/SkipPrevious";
+import PlayArrowIcon from "@material-ui/icons/PlayArrow";
+import SkipNextIcon from "@material-ui/icons/SkipNext";
+
+const styles = theme => ({
+  card: {
+    display: "flex"
+  },
+  details: {
+    display: "flex",
+    flexDirection: "column"
+  },
+  content: {
+    flex: "1 0 auto"
+  },
+  cover: {
+    width: 151
+  },
+  controls: {
+    display: "flex",
+    alignItems: "center",
+    paddingLeft: theme.spacing.unit,
+    paddingBottom: theme.spacing.unit
+  },
+  playIcon: {
+    height: 38,
+    width: 38
+  }
+});
+
+function MediaControlCard(props) {
+  const { classes, theme } = props;
+
+  return (
+    <Card className={classes.card}>
+      <CardMedia
+        className={classes.cover}
+        image={props.data.snippet.thumbnails.default.url}
+        title="Live from space album cover"
+      />
+      <div className={classes.details}>
+        <CardContent className={classes.content}>
+          <Typography component="h5" variant="h5">
+            {props.data.snippet.channelTitle}
+          </Typography>
+          <Typography variant="subtitle1" color="textSecondary">
+            {props.data.snippet.description}
+          </Typography>
+        </CardContent>
+      </div>
+      {console.log(props.data.snippet)}
+    </Card>
+  );
+}
+
+MediaControlCard.propTypes = {
+  classes: PropTypes.object.isRequired,
+  theme: PropTypes.object.isRequired
+};
+
+export default withStyles(styles, { withTheme: true })(MediaControlCard);
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,21 +2,80 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import { logoutUser } from "../actions";
 
+import NavbarPage from './NavbarLinks/NavbarPage';
+
+import IconButton from "@material-ui/core/IconButton";
+import InputAdornment from "@material-ui/core/InputAdornment";
+import SearchIcon from "@material-ui/icons/Search";
+import TextField from '@material-ui/core/TextField';
+import api, { baseParams } from '../api';
+
+import Cards from './Cards';
+
 class Home extends Component {
+
+  state = {
+    search: '',
+    channels: [],
+  };
+
   handleLogout = () => {
     const { dispatch } = this.props;
     dispatch(logoutUser());
   };
+
+  handleChange = (e) => {
+    this.setState({ [e.target.id]: e.target.value });
+  };
+
+  handleClick = async () => {
+    console.log(this.state.search);
+    const response = await api.get('/search', {
+      params: {
+        ...baseParams,
+        q: this.state.search
+      }
+    });
+
+    this.setState({ channels: response.data.items }, () => { this.filterData() });
+
+  }
+
+
+  filterData = () => {
+    console.log(this.state.channels);
+  }
+
   render() {
     const { isLoggingOut, logoutError } = this.props;
 
     return (
       <div>
-        <h1>This is your app's protected area.</h1>
-        <p>Any routes here will also be protected</p>
-        <button onClick={this.handleLogout}>Logout</button>
+        <NavbarPage />
+        <div className="App-header">
+          <TextField
+            label="Search your youtuber"
+            InputProps={{
+              endAdornment: (
+                <InputAdornment>
+                  <IconButton>
+                    <SearchIcon onClick={this.handleClick} />
+                  </IconButton>
+                </InputAdornment>
+              )
+            }}
+            onChange={this.handleChange}
+            id='search'
+          />
+          {(this.state.channels).map((n) => {
+            return (
+              <Cards data={n}/>
+            )
+          })}
+        </div>
+        {/* <button onClick={this.handleLogout}>Logout</button>
         {isLoggingOut && <p>Logging Out....</p>}
-        {logoutError && <p>Error logging out</p>}
+        {logoutError && <p>Error logging out</p>} */}
       </div>
     );
   }
